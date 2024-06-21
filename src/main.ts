08f2219773d615de2d2cff937d949b812111521b
gurwoghd <gurwoghd@gmail.com>
2024-06-21 16:54:28 +0900

    chore: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  // set swagger config
+  const config = new DocumentBuilder()
+    .setTitle('library reservation API')
+    .setDescription('library reservation API')
+    .setVersion('1.0')
+    .addTag('library reservation')
+    .addOAuth2(
+      {
+        type: 'oauth2',
+        scheme: 'bearer',
+        in: 'header',
+        bearerFormat: 'token',
+        flows: {
+          authorizationCode: {
+            authorizationUrl: configService.getOrThrow('SWAGGER_AUTH_URL'),
+            tokenUrl: configService.getOrThrow('SWAGGER_TOKEN_URL'),
+            scopes: {
+              openid: 'openid',
+              email: 'email',
+              profile: ' profile',
+            },
+          },
+        },
+      },
+      'oauth2',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      oauth2RedirectUrl: `${configService.getOrThrow('API_URL')}/api/oauth2-redirect.html`,
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
